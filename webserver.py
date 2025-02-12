a5ac21e936c4110f367e8d23106698c5c17d191a
m.moenter <m.moenter@basecom.de>
2025-02-12 08:52:38 +0100

    finished Todolistenverwaltung

--- webserver.py
+++ webserver.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/todo-lists', methods=['POST'])
+@app.route('/todo-list', methods=['POST'])
 
-def todoLists():
+def addTodoList():
     response = jsonify("todoList")
     return response
 
